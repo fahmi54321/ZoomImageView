313ac5f79664ef26b5f24c356fc950eb87213eb4
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-01 12:20:33 +0700

    zoom image view

--- app/src/main/java/com/android/a4zoomimageview/MainActivity.java
+++ app/src/main/java/com/android/a4zoomimageview/MainActivity.java
@@ -3,12 +3,21 @@ package com.android.a4zoomimageview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.image);
+        PhotoViewAttacher photoViewAttacher = new PhotoViewAttacher(imageView);
+        photoViewAttacher.update();
     }
 }
